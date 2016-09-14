5617294662e9f38ffb6c225ca22935c5b71a7caf
ksavransky <ksavransky@gmail.com>
2016-09-14 10:34:15 -0700

    ai working more smoothly but doesn't lose

--- superpong.js
+++ superpong.js
@@ -6,13 +6,15 @@ var compScore = 0;
 
 var ballX = canvas.width/2;
 var ballY = canvas.height/2;
-var ballDX = 1;
-var ballDY = 2;
+var ballDX = 2;
+var ballDY = 3;
 var ballRadius = 10;
 
 var paddleHeight = 10;
 var paddleWidth = 75;
+
 var paddleX = (canvas.width-paddleWidth)/2;
+var compPaddleX = (canvas.width-paddleWidth)/2;
 
 var rightPressed = false;
 var leftPressed = false;
@@ -21,6 +23,14 @@ function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawCompPaddle() {
+    ctx.beginPath();
+    ctx.rect(compPaddleX, 0, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
 }
@@ -37,6 +47,7 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawCompPaddle();
 
     if(ballX + ballDX > canvas.width-ballRadius
       || ballX + ballDX < ballRadius) {
@@ -51,27 +62,38 @@ function draw() {
         compScore++;
         ballX = canvas.width/2;
         ballY = canvas.height/2;
-        ballDX = 1;
-        ballDY = 2;
+        ballDX = -ballDX;
+        ballDY = 3;
       }
     }
 
-    if(ballY + ballDY < ballRadius) {
-      humanScore++;
-      ballX = canvas.width/2;
-      ballY = canvas.height/2;
-      ballDX = 1;
-      ballDY = 2;
+    if(ballY + ballDY < ballRadius + paddleHeight) {
+      if(ballX > compPaddleX && ballX < compPaddleX + paddleWidth) {
+        ballDY = -ballDY;
+        }
+        else {
+        humanScore++;
+        ballX = canvas.width/2;
+        ballY = canvas.height/2;
+        ballDX = -ballDX;
+        ballDY = 3;
+      }
     }
 
     ballX += ballDX;
     ballY += ballDY;
 
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
+      paddleX += 7;
     }
     else if(leftPressed && paddleX > 0) {
-        paddleX -= 7;
+      paddleX -= 7;
+    }
+
+    if(ballX > compPaddleX && compPaddleX < canvas.width-paddleWidth){
+      compPaddleX += 2;
+    } else if (ballX-paddleWidth < compPaddleX){
+      compPaddleX -= 2;
     }
 }
 
